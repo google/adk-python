5028d2a64523b9c932d75c842e6478e0317f4e01
hangfei <hangfeilin@gmail.com>
2025-06-11 17:23:54 -0700

    fix: remove activity detection as it needs proper design

--- src/google/adk/models/gemini_llm_connection.py
+++ src/google/adk/models/gemini_llm_connection.py
@@ -92,8 +92,7 @@ class GeminiLlmConnection(BaseLlmConnection):
       )
 
   async def send_realtime(
-      self, blob: types.Blob, automatic_activity_detection: bool = True
-  ):
+      self, blob: types.Blob):
     """Sends a chunk of audio or a frame of video to the model in realtime.
 
     Args:
@@ -103,16 +102,7 @@ class GeminiLlmConnection(BaseLlmConnection):
 
     input_blob = blob.model_dump()
     logger.debug('Sending LLM Blob: %s', input_blob)
-    if automatic_activity_detection:
-      await self._gemini_session.send_realtime_input(audio=input_blob)
-    else:
-      await self._gemini_session.send_realtime_input(
-          activity_start=types.ActivityStart()
-      )
-      await self._gemini_session.send_realtime_input(audio=input_blob)
-      await self._gemini_session.send_realtime_input(
-          activity_end=types.ActivityEnd()
-      )
+    await self._gemini_session.send_realtime_input(audio=input_blob)
 
   def __build_full_text_response(self, text: str):
     """Builds a full text response.
