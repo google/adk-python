a78e82e01386cf43dfb4be7c0acf79d5f09fe6b6
J-Dream <30745956+J-Dream@users.noreply.github.com>
2025-06-16 00:55:42 -0700

    Restore AGENT-DEVELOPMENT-KIT-CRASH-COURSE directory from local backup

---
+++ AGENT-DEVELOPMENT-KIT-CRASH-COURSE/1-basic-agent/greeting_agent/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="greeting_agent",
+    # https://ai.google.dev/gemini-api/docs/models
+    model= "gemini-2.0-flash-exp",# "gemini-2.0-flash-live-preview-04-09",  #"gemini-2.0-flash-exp", 
+    description="Greeting agent",
+    instruction="""
+    You are an interesting and engaging conversationalist. you will ask the users name and instegate a conversation with 
+    user.
+    """,
+)
