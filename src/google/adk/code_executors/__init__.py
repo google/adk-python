760fb7f39f6fe6af4c1e6d48d578ca2449a8c45b
Summer <xiaotongyang@google.com>
2025-06-24 19:22:04 +0000

    [06/24] Add gke_code_executor.py

--- src/google/adk/code_executors/__init__.py
+++ src/google/adk/code_executors/__init__.py
@@ -56,7 +56,6 @@ def __getattr__(name: str):
   elif name == 'GkeCodeExecutor':
     try:
       from .gke_code_executor import GkeCodeExecutor
-
       return GkeCodeExecutor
     except ImportError as e:
       raise ImportError(
