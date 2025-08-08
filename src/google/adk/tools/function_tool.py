26e42185d49f9db0767559e7deffce64ebde2bb3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-08 16:20:33 -0400

    chore: Fix spelling - function
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/google/adk/tools/function_tool.py
+++ src/google/adk/tools/function_tool.py
@@ -224,7 +224,7 @@ You could retry calling this tool, but it is IMPORTANT for you to provide all th
 
     # Functions are callable objects, but not all callable objects are functions
     # checking coroutine function is not enough. We also need to check whether
-    # Callable's __call__ function is a coroutine funciton
+    # Callable's __call__ function is a coroutine function
     is_async = inspect.iscoroutinefunction(target) or (
         hasattr(target, '__call__')
         and inspect.iscoroutinefunction(target.__call__)
