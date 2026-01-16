bce8a8e30cda488a62007478fad2f866bdeb4c5d
Lin-Nikaido <sct.mth.1021@gmail.com>
2026-01-16 12:08:26 +0900

    resolve

--- src/google/adk/agents/readonly_context.py
+++ src/google/adk/agents/readonly_context.py
@@ -24,6 +24,7 @@ if TYPE_CHECKING:
 
   from ..sessions.session import Session
   from .invocation_context import InvocationContext
+  from .run_config import RunConfig
 
 
 class ReadonlyContext:
@@ -56,5 +57,15 @@ class ReadonlyContext:
 
   @property
   def session(self) -> Session:
-    """The current session. READONLY field."""
+    """The current session for this invocation."""
     return self._invocation_context.session
+
+  @property
+  def user_id(self) -> str:
+    """The id of the user. READONLY field."""
+    return self._invocation_context.user_id
+
+  @property
+  def run_config(self) -> Optional[RunConfig]:
+    """The run config of the current invocation. READONLY field."""
+    return self._invocation_context.run_config
