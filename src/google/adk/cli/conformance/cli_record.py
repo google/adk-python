76af7118713e98b1b1fed0729ec9eb54717412f7
Sara Robinson <sararob@google.com>
2026-01-27 11:11:09 -0500

    Revert test change

--- src/google/adk/cli/conformance/cli_record.py
+++ src/google/adk/cli/conformance/cli_record.py
@@ -51,7 +51,6 @@ async def _create_conformance_test_files(
         state=test_case.test_spec.initial_state,
     )
 
-
     # Run the agent with the user messages
     function_call_name_to_id_map = {}
     for user_message_index, user_message in enumerate(
