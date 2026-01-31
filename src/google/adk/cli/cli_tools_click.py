ae57001529866724914df0c956b07682ac1598ec
ftnext <takuyafjp+develop@gmail.com>
2026-01-31 13:32:48 +0900

    fix: Escape Click’s Wrapping in adk deploy agent_engine example
    https://click.palletsprojects.com/en/stable/documentation/#escaping-click-s-wrapping

--- src/google/adk/cli/cli_tools_click.py
+++ src/google/adk/cli/cli_tools_click.py
@@ -1917,9 +1917,11 @@ def cli_deploy_agent_engine(
 
   Example:
 
+    \b
     # With Express Mode API Key
     adk deploy agent_engine --api_key=[api_key] my_agent
 
+    \b
     # With Google Cloud Project and Region
     adk deploy agent_engine --project=[project] --region=[region]
       --display_name=[app_name] my_agent
