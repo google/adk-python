2eec63bef9e5bc5609fd6796be0a074b0b7c93f5
RobertoLuzanilla <luzanillasanchez@gmail.com>
2026-05-08 16:03:06 -0700

    chore: add license header to config agent utils tests

--- tests/unittests/agents/test_config_agent_utils.py
+++ tests/unittests/agents/test_config_agent_utils.py
@@ -1,5 +1,20 @@
-from google.adk.agents.config_agent_utils import check_config_for_blocked_keys
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
+
+from google.adk.agents.config_agent_utils import check_config_for_blocked_keys
 
 
 def test_check_config_for_blocked_keys_rejects_args_key():
